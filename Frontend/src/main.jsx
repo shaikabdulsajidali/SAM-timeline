271d412984e825f4cfa8afa98c315461245b54ab
SHAIK ABDUL SAJID ALI <146320402+shaikabdulsajidali@users.noreply.github.com>
2026-01-19 16:57:09 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,3 +1,9 @@
+// Copyright (c) 2026 shaikabdulsajidali
+// Licensed under the MIT License
+// See LICENSE file in the project root for full license text
+
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'

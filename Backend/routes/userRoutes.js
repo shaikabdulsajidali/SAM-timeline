102cd282200cd1883f81d9711bb662d391701f51
SHAIK ABDUL SAJID ALI <146320402+shaikabdulsajidali@users.noreply.github.com>
2026-01-19 17:00:11 +0530

    Update userRoutes.js

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,3 +1,9 @@
+// Copyright (c) 2026 shaikabdulsajidali
+// Licensed under the MIT License
+// See LICENSE file in the project root for full license text
+
+
+
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
